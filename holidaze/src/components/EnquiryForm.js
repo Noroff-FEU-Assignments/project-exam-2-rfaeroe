7b3f5e92c3c511d7bc23750475f2d381c418b29e
rfaeroe <rune.faeroe@gmail.com>
2021-05-22 23:37:55 +0200

    Created Enquiries form

--- holidaze/src/components/EnquiryForm.js
+++ holidaze/src/components/EnquiryForm.js
@@ -1,12 +1,9 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { enquiriesSchema } from '../utils/schemas';
 import useAxios from '../utils/useAxios';
 import { BASE_URL, ESTABLISHMENTS_PATH } from '../utils/constants';
-import {useFormik} from 'formik';
-import * as yup from 'yup'; 
+import { useFormik } from 'formik';
+import * as yup from 'yup';
 
 
 const EnquiryForm = () => {
@@ -67,21 +64,26 @@ const EnquiryForm = () => {
         return <p>loading product</p>;
     }
 
+
     return (
 
         <div className={""}>
-            <h1 className={"pageheading"}>Book {establishment.establishment_name}</h1>
-
             <form className='form' onSubmit={handleSubmit}>
-                {updateError && <p>{updateError}</p>}
+                <p className='form-title'>Booking enquiry</p>
+
+                <div className='form-group col-d-12 col-m-12'>
+                    {updateError && <p className="message error">{updateError}</p>}
+                </div>
+                <div className='form-group col-d-12 col-m-12'>
+                    {success ? <p className="message success">Booking enquiry for {establishment.establishment_name} was sent</p> : null}
+                </div>
                 <fieldset className='row' disabled={submitting}>
                     <div className='form-group col-d-12 col-m-12'>
                         <input
-                            name='name'
+                            name='establishment_name'
                             placeholder='Name'
                             onChange={handleChange}
                             defaultValue={establishment.establishment_name}
-                            
                         />
                         {errors.name && <p>{errors.name.message}</p>}
                     </div>
@@ -113,6 +115,7 @@ const EnquiryForm = () => {
                             value={values.children}
                             onChange={handleChange}
                             type='number'
+                            min="0"
                         />
                         {errors.children && <p>{errors.children.message}</p>}
                     </div>
@@ -123,6 +126,7 @@ const EnquiryForm = () => {
                             onChange={handleChange}
                             value={values.adults}
                             type='number'
+                            min="0"
                         />
                         {errors.adults && <p>{errors.adults.message}</p>}
                     </div>
@@ -131,7 +135,7 @@ const EnquiryForm = () => {
                     </div>
                 </fieldset>
             </form>
-            {success ? <p>Booking enquiry for {establishment.establishment_name} was sent</p> : null}
+
         </div>
     );
 };
